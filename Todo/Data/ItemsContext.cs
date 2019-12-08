92ebce6fb0be736ee9aaaee739365a28b9cd9ede
Иван Хомичков <ivan.khomichkov@gmail.com>
2019-12-08 22:12:17 +0300

    Initial commit

---
+++ Todo/Data/ItemsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Data
+{
+    public class ItemsContext : DbContext
+    {
+        
+    }
+}
---
+++ Todo/Model/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Todo.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Due { get; set; }
+        
+    }
+}
